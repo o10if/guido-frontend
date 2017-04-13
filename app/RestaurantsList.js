9111092eb913d15ea1ab75d9a8acc5bacb711d6a
gaoyiqin95 <yiqin.gao@insa-lyon.fr>
2017-04-13 17:55:27 +0800

    PathList View v2 TODO: resize the icons

--- app/RestaurantsList.js
+++ app/RestaurantsList.js
@@ -104,9 +104,6 @@ class RestaurantsList extends Component {
                   <Caption styleName="collapsible" numberOfLines={2}>{path.description}</Caption>
                 </View>
                 <View styleName="horizontal">
-                  <Caption styleName="collapsible" numberOfLines={2}> </Caption>
-                </View>
-                <View styleName="horizontal">
                   <View styleName="horizontal flexible">
                     <Icon style={{color: 'gray', flex:1}} name="like" />
                     <Caption style={{color: 'gray', flex:1}} >{path.like}</Caption>
