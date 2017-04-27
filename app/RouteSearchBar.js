caeacfe1510a743025e0fb605c9d04240dce9319
gaoyiqin95 <yiqin.gao@insa-lyon.fr>
2017-04-28 06:32:56 +0800

    Finish design search bar. TODO: response to research

---
+++ app/RouteSearchBar.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text, TextInput, View } from 'react-native';
+import {
+  Icon
+} from '@shoutem/ui';
+
+export default class RouteSearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+
+  render() {
+    return (
+      <View>
+        <View style={{flex: 1, borderColor: '#E0E0E0', backgroundColor: '#E0E0E0', borderWidth: 1, borderRadius: 10, flexDirection: 'row'}}>
+          <Icon
+            style={{color:'gray', paddingLeft:5, paddingTop:2}}
+            name="search" />
+          <TextInput
+            returnKeyType = "search"
+            underlineColorAndroid='transparent'
+            style={{flex: 1, height: 30, fontSize: 15, paddingBottom: 5}}
+            placeholder="Type here to search route"
+            onChangeText={(text) => this.setState({text})}
+          />
+        </View>
+        <Text style={{fontSize: 42}}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+        </Text>
+      </View>
+    );
+  }
+}
