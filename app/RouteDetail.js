0f6946cb5170de54b10f862857fce4f75d3b0f64
Paul-Louis <paul-louis.hery@insa-lyon.fr>
2017-04-29 22:02:43 +0200

    Improve RouteDetail

--- app/RouteDetail.js
+++ app/RouteDetail.js
@@ -18,12 +18,27 @@ import {
   Caption,
   TextInput
 } from '@shoutem/ui';
+import {Alert} from "react-native";
 
 mapStyle = [];// require('../assets/mapStyle.json');
 
+
+coordinates = [{latitude: 45.7570142556995, longitude: 4.832353591918945},{latitude: 45.7579424547621, longitude: 4.833769798278809},{latitude: 45.760427556887024, longitude: 4.834005832672119},{latitude: 45.760697019140615, longitude: 4.834027290344238},{latitude: 45.76062216864514, longitude: 4.836945533752441},{latitude: 45.76172994572358, longitude: 4.836945533752441},{latitude: 45.76217903827326, longitude: 4.837031364440918},{latitude: 45.763466416880846, longitude: 4.836387634277343},{latitude: 45.76345144753286, longitude: 4.835700988769531},{latitude: 45.763331692604346, longitude: 4.834156036376953},{latitude: 45.762074250335004, longitude: 4.834113121032715},{latitude: 45.76202934115837, longitude: 4.832825660705566},{latitude: 45.76198443194557, longitude: 4.832439422607422},{latitude: 45.762074250335004, longitude: 4.831752777099609},{latitude: 45.763226906831036, longitude: 4.83198881149292},{latitude: 45.763226906831036, longitude: 4.829435348510742},{latitude: 45.761370669081074, longitude: 4.828770160675049},{latitude: 45.761939522696615, longitude: 4.826087951660156},{latitude: 45.76036767620949, longitude: 4.8255085945129395},{latitude: 45.760322765659154, longitude: 4.826667308807372},{latitude: 45.759933539375425, longitude: 4.826602935791016},{latitude: 45.75966407343458, longitude: 4.826366901397705},{latitude: 45.75905028504455, longitude: 4.826409816741943},{latitude: 45.75875087362366, longitude: 4.8268818855285645},{latitude: 45.75740350234746, longitude: 4.825594425201416},{latitude: 45.75678968909056, longitude: 4.827032089233398},{latitude: 45.756041127200405, longitude: 4.8262810707092285},{latitude: 45.75569678535898, longitude: 4.827332496643066},{latitude: 45.75512786809614, longitude: 4.828941822052002},{latitude: 45.75575667104927, longitude: 4.829435348510742},{latitude: 45.7555620423209, longitude: 4.8301005363464355},{latitude: 45.75530752680575, longitude: 4.830679893493652},{latitude: 45.75366063365148, longitude: 4.829607009887695},{latitude: 45.75346599761287, longitude: 4.83048677444458},{latitude: 45.7557416996327, longitude: 4.831645488739014},{latitude: 45.75602615586017, longitude: 4.8310017585754395},{latitude: 45.75675974680774, longitude: 4.8314738273620605},{latitude: 45.75696934244999, longitude: 4.832096099853516}];
+
 export default class RouteDetail extends Component {
+
   static propTypes = {
     restaurant: React.PropTypes.object,
+  };
+
+  _todoFeature() {
+    Alert.alert(
+      'Fonctionnalité non implémentée',
+      'Cette fonctionalité arrive très bientot dans votre application !',
+      [
+        {text: 'OK', onPress: () => console.log('OK Pressed')},
+      ]
+    );
   };
 
   render() {
@@ -31,12 +46,6 @@ export default class RouteDetail extends Component {
 
     return (
       <Screen styleName="paper full-screen">
-        <NavigationBar
-          title="Parcours StreetArt"
-          styleName=""
-        />
-
-        <ScrollView>
           <MapView
             style={{alignSelf: 'stretch', height: 300}}
             initialRegion={{
@@ -46,7 +55,13 @@ export default class RouteDetail extends Component {
               longitudeDelta: 0.008
             }}
             customMapStyle={mapStyle}
-          />
+          >
+            <MapView.Polyline
+              coordinates={coordinates}
+              strokeColor="#2b2c2c"
+              strokeWidth={2}
+            />
+          </MapView>
           <Screen styleName="paper">
 
             <Row>
@@ -55,8 +70,12 @@ export default class RouteDetail extends Component {
                 <Subtitle>Parcours StreetArt</Subtitle>
                 <Text>1.2km - 19min</Text>
               </View>
-              <Icon name="add-to-favorites" />
-              <Icon name="comment" />
+              <Button onPress={this._todoFeature}>
+                <Icon name="add-to-favorites" />
+              </Button>
+              <Button styleName="tight" onPress={this._todoFeature}>
+                <Icon name="comment" />
+              </Button>
             </Row>
 
             <Divider styleName="line" />
@@ -64,7 +83,7 @@ export default class RouteDetail extends Component {
             <Text styleName="md-gutter multiline">Le parcours streetart permet de découvrir les lieux mythiques du streetart à Lyon. Il dure 19 minutes.
             </Text>
 
-            <Button  styleName="dark">
+            <Button styleName="dark" onPress={this._todoFeature}>
               <Icon name="play" />
               <Text>Démarrer la navigation</Text>
             </Button>
@@ -136,7 +155,6 @@ export default class RouteDetail extends Component {
               </View>
             </Row>
           </Screen>
-        </ScrollView>
       </Screen>
     );
   }
