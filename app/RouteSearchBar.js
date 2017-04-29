456c7f57fe6c6f8d7dba631d9b641ddebcaa6544
gaoyiqin95 <yiqin.gao@insa-lyon.fr>
2017-04-30 07:04:46 +0800

    Response to research done

--- app/RouteSearchBar.js
+++ app/RouteSearchBar.js
@@ -4,6 +4,9 @@ import {
   Icon
 } from '@shoutem/ui';
 
+import RoutesList from './RoutesList';
+import SearchResult from './SearchResult';
+
 export default class RouteSearchBar extends Component {
   constructor(props) {
     super(props);
@@ -13,7 +16,7 @@ export default class RouteSearchBar extends Component {
   render() {
     return (
       <View>
-        <View style={{flex: 1, borderColor: '#E0E0E0', backgroundColor: '#E0E0E0', borderWidth: 1, borderRadius: 5, flexDirection: 'row'}}>
+        <View style={{flex: 1, borderColor: '#E0E0E0', backgroundColor: '#E0E0E0', borderWidth: 1, borderRadius: 10, flexDirection: 'row'}}>
           <Icon
             style={{color:'gray', paddingLeft:5, paddingTop:2}}
             name="search" />
@@ -22,12 +25,14 @@ export default class RouteSearchBar extends Component {
             underlineColorAndroid='transparent'
             style={{flex: 1, height: 30, fontSize: 15, paddingBottom: 5}}
             placeholder="Type here to search route"
+            value={this.state.text}
             onChangeText={(text) => this.setState({text})}
           />
         </View>
-        <Text style={{fontSize: 42}}>
-          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-        </Text>
+
+        {
+          this.state.text==''?<RoutesList />:<SearchResult text={this.state.text.split(' ').map((word) => word && '🍕').join(' ')}/>
+        }
       </View>
     );
   }
