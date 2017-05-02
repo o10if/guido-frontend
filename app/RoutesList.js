d1ec0513539435f97059efb14da8a18ed8ea1e54
Mehdi BAHA <elmehdi.baha@gmail.com>
2017-05-02 12:18:26 +0200

    Add link to backend for RoutesList

--- app/RoutesList.js
+++ app/RoutesList.js
@@ -1,110 +1,90 @@
+import { Route } from './agent';
+
 import React, { Component } from 'react';
 import {
-  Icon,
-  Tile,
-  Divider,
-  Screen,
-  ListView,
   Image,
-  View,
-  Text,
-  Title,
+  ListView,
   Subtitle,
   TouchableOpacity,
+  Screen,
+  Text,
+  GridRow,
+  Card,
+  View,
   Caption,
+  Icon,
 } from '@shoutem/ui';
 
-import RouteDetail from './RouteDetail';
-
-export default class RouteList extends Component {
+export default class RoutesList extends Component {
 
   constructor(props) {
+    console.warn("constructor");
     super(props);
-    this.renderRow = this.renderRow.bind(this);
     this.state = {
-      results: [{
-        "id": "a4b76c9ef67dbf",
-        "name": "Trajet1",
-        "description": "trajet1 trajet1 trajet1 trajet1 trajet1 trajet1",
-        "image": { "url": "http://ortholudo.s3.amazonaws.com/production/locations/pictures/000/000/037/original/lyon.jpeg" },
-        "like": "325",
-        "comments": "626",
-      }, {
-        "id": "c23fb35ecceafb",
-        "name": "Trajet2",
-        "description": "trajet2 trajet2 trajet2 trajet2 trajet2 trajet2",
-        "image": { "url": "http://static.vueling.com/cms/media/1216826/lyon.jpg" },
-        "like": "875",
-        "comments": "623",
-      }, {
-        "id": "c142e53d23aec",
-        "name": "Trajet3",
-        "description": "trajet3 trajet3 trajet3 trajet3 trajet3 trajet3",
-        "image": { "url": "http://lyon-sortie.fr/wp-content/uploads/sites/116/2016/09/Lyon.jpg" },
-        "like": "290",
-        "comments": "315",
-      }, {
-        "id": "cedfbcfaebfae",
-        "name": "Trajet4",
-        "description": "trajet4 trajet4 trajet4 trajet4 trajet4 trajet4",
-        "image": { "url": "http://ortholudo.s3.amazonaws.com/production/locations/pictures/000/000/037/original/lyon.jpeg" },
-        "like": "98",
-        "comments": "24",
-      }],
+      routes: [],
+      isLoading: true
     }
   }
 
-  rowPressed(result) {
-      var id = result.id;
-      var name = result.name;
-
-      this.props.navigator.push({
-        title: name,
-        name: 'RouteDetail',
-        component: RouteDetail,
-        passProps: {id: id}
-      });
+  async componentDidMount() {
+    console.warn("componentDidMount");
+    let routes = await Route.all();
+    console.warn(routes.toString());
+    this.setState({
+      routes: routes,
+      isLoading: false
+    });
+    console.warn("component ->  " + this.state.isLoading);
   }
 
-  renderRow(result) {
+  renderRow(rowData, sectionId, index) {
+    console.warn("renderRow");
+    const cellViews = rowData.map((path, id) => {
     return (
-      <TouchableOpacity
-        style={{flex:1}}
-        onPress={()=>this.rowPressed(result)}>
-        <View style={{flex:1}}>
-          <Image
-            styleName="large-banner"
-            source={{ uri: result.image.url }}
-          >
-            <Tile>
-              <Title styleName="md-gutter-bottom">{result.name} {result.id}</Title>
-              <Subtitle styleName="sm-gutter-horizontal">{result.description}</Subtitle>
-            </Tile>
-            <View styleName="horizontal" style={{marginTop:150, marginLeft:230}}>
-                            <View styleName="horizontal flexible">
-                              <Icon style={{color: 'white', flex:1, top:3, transform:[{scale:0.75}]}} name="like" />
-                              <Caption style={{color: 'white', flex:1}} >{result.like}</Caption>
-                            </View>
-                            <View styleName="horizontal flexible">
-                              <Icon style={{color: 'white', flex:1, top:4.5, transform:[{scale:0.7}]}}  name="comment-full" />
-                              <Caption style={{color: 'white', flex:1}} >{result.comments}</Caption>
-                            </View>
-                          </View>
-          </Image>
-          <Divider styleName="line" />
-        </View>
-      </TouchableOpacity>
+        <TouchableOpacity key={id} styleName="flexible">
+          <Card styleName="flexible">
+            <Image styleName="medium-wide" source={{uri: path.image && path.image.url}} />
+            <View styleName="content">
+              <Subtitle numberOfLines={3}>{path.name}</Subtitle>
+              <View styleName="horizontal">
+                <Caption styleName="collapsible" numberOfLines={2}>{path.description}</Caption>
+              </View>
+              <View styleName="horizontal">
+                <Caption styleName="collapsible" numberOfLines={2}> </Caption>
+              </View>
+              <View styleName="horizontal">
+                <View styleName="horizontal flexible">
+                  <Icon style={{color: 'gray', flex:1, top:3, transform:[{scale:0.75}]}} name="like" />
+                  <Caption style={{color: 'gray', flex:1}} >{path.like}</Caption>
+                </View>
+                <View styleName="horizontal flexible">
+                  <Icon style={{color: 'gray', flex:1, top:4.5, transform:[{scale:0.7}]}}  name="comment-full" />
+                  <Caption style={{color: 'gray', flex:1}} >{path.comments}</Caption>
+                </View>
+              </View>
+            </View>
+          </Card>
+        </TouchableOpacity>
+      );
+    });
+    return (
+      <GridRow columns={2}>
+        {cellViews}
+      </GridRow>
     );
   }
 
   render() {
+    console.warn("loading: " + this.state.isLoading);
+    const groupedData = GridRow.groupByRows(this.state.routes, 2, () => {
+      return 1;
+    });
     return (
-      <Screen style={{flex:1}}>
-        <ListView
-          data={this.state.results}
-          renderRow={this.renderRow}
-        />
-      </Screen>
+      <ListView
+        data={groupedData}
+        loading={this.state.isLoading}
+        renderRow={this.renderRow}
+      />
     );
   }
 }
