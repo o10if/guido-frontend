e4df9030a6a8bf489c2bf283542f280625f413a6
antonrautio <anton.rautio@insa-lyon.fr>
2017-05-02 12:03:38 +0200

    Functioning front-end search

--- app/Search.js
+++ app/Search.js
@@ -7,41 +7,37 @@ import {
 
 import {
   Icon,
-  Row
+  Row,
+  Image,
+  Tile,
+  Title,
+  Divider,
+  Subtitle
 } from '@shoutem/ui';
 
 import SearchBar from 'react-native-searchbar';
 
 const items = [
-  1337,
-  1332,
-  13,
-  1,
-  'Trajet1',
-  'Trajet2',
-  'Trajet3',
-  'Trajet4',
-
-  'janeway',
-  {
-    lots: 'of',
-    different: {
-      types: 0,
-      data: false,
-      that: {
-        can: {
-          be: {
-            quite: {
-              complex: {
-                hidden: [ 'gold!' ],
-              },
-            },
-          },
-        },
-      },
-    },
-  },
-  [ 4, 2, 'tree' ],
+{
+  "title": "Street art",
+  "subtitle": "A walk by the best graffiti spots of Lyon",
+  "image": { "url": "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTdTw0nPNXmis_5a0QZ1Hd3VhK2hPQYUDOALjqL70982qq2B-_N" }
+},
+{
+  "title": "History",
+  "subtitle": "See all the important historical sites of Lyon",
+  "image": { "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmypAfm3oVWNHtZ1GcRhpo6uu8psEn_pcrvApQGQ2jnjUT2NVA" }
+},
+{
+  "title": "By the Rhone",
+  "subtitle": "Refreshing walk next to the river",
+  "image": { "url": "https://bonjourparis.com/wp-content/uploads/2015/05/cotes-du-rhone-nord.jmarnaud.flkr_.jpg" }
+},
+{
+  "title": "INSA Campus",
+  "subtitle": "See the campus area of INSA in its greatness",
+  "image": { "url": "https://i.ytimg.com/vi/EvA2H9zf0Ls/maxresdefault.jpg" }
+}
 ];
 
 export default class SearchContainer extends Component {
@@ -63,24 +59,35 @@ export default class SearchContainer extends Component {
     return (
       <View>
         <View style={{ marginTop: 50 }}>
-          {
-            this.state.results.map((result, i) => {
-              return (
-                <TouchableOpacity onPress={() => this.searchBar.hide()}>
-                  <Row>
-                    <Icon name="tweet" />
-                    <Text key={i}>{result.toString()}</Text>
-                  </Row>
-                </TouchableOpacity>
-              );
-            })
-          }
           <TouchableOpacity onPress={() => this.searchBar.show()}>
             <Icon name="search" />
           </TouchableOpacity>
           <TouchableOpacity onPress={() => this.searchBar.hide()}>
             <Icon name="close" />
           </TouchableOpacity>
+        </View>
+        <View>
+          {
+            this.state.results.map((result, i) => {
+              return (
+                <TouchableOpacity onPress={() => this.searchBar.hide()}>
+                    <View>
+                      <Image
+                        style={{ width: 355, height: 100}}
+                        source={{ uri: result.image.url }}
+                      >
+                        <Tile>
+                          <Title>{result.title}</Title>
+                          <Subtitle>{result.subtitle}</Subtitle>
+                        </Tile>
+                      </Image>
+                      <Divider styleName="line" />
+                    </View>
+                </TouchableOpacity>
+              );
+            })
+          }
+
         </View>
         <SearchBar
           ref={(ref) => this.searchBar = ref}
