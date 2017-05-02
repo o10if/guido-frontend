6e1cdefd7cd81d87f11ddc806158fc46c98ba2a8
Mehdi BAHA <elmehdi.baha@gmail.com>
2017-05-02 12:18:26 +0200

    Add link to backend for RoutesList

---
+++ app/agent.js
@@ -0,0 +1,48 @@
+const API_ROOT = 'https://guido-backend.herokuapp.com';
+
+async function makeRequest(url, params, method) {
+  try {
+    let response = await fetch(`${API_ROOT}${url}`, {
+      method: method,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(params)
+    });
+    let responseJson = await response.json();
+    return responseJson;
+  } catch(error) {
+    console.error(error);
+  }
+}
+
+export const Route = {
+  all: () =>
+    makeRequest(`/routes`),
+  get: (id) =>
+    makeRequest(`/routes/${id}`),
+  post: (id, route) =>
+    makeRequest(`/routes/${id}`, route, 'POST')
+};
+
+// Careful: maybe not implemented
+export const Comment = {
+  get: (id) =>
+    makeRequest(`/comments/${id}`),
+};
+
+export const Event = {
+  get: (id) =>
+    makeRequest(`/events/${id}`),
+};
+
+export const Waypoint = {
+  get: (id) =>
+    makeRequest(`/waypoints/${id}`),
+};
+
+export const Landmark = {
+  get: (id) =>
+    makeRequest(`/landmarks/${id}`),
+};
