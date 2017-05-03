946675aff8b161b089bb01980200245bb3c09974
Paul-Louis <paul-louis.hery@insa-lyon.fr>
2017-05-03 18:36:19 +0200

    Fix navbar + sluggishness

--- app/App.js
+++ app/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import SearchHome from './SearchHome';
+import AppNavigator from './AppNavigator';
 
 export default React.createClass({
   render() {
-    return <SearchHome />
+    return <AppNavigator />
   },
 });
